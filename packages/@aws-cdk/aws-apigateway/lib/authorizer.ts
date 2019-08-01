148d50cbf34f5a6b56b1c9266f42436b2e04ef01
Iain Cole <iain@twitch.tv>
2019-08-01 16:18:37 -0700

    Add apigateway.Authorizer

--- packages/@aws-cdk/aws-apigateway/lib/authorizer.ts
+++ packages/@aws-cdk/aws-apigateway/lib/authorizer.ts
@@ -1,3 +1,6 @@
+import { Construct } from '@aws-cdk/core';
+import { CfnAuthorizer, CfnAuthorizerProps } from './apigateway.generated';
+
 /**
  * Represents an API Gateway authorizer.
  */
@@ -6,4 +9,20 @@ export interface IAuthorizer {
    * The authorizer ID.
    */
   readonly authorizerId: string;
+}
+
+/**
+ * A Authorizer of a REST API.
+ */
+export class Authorizer extends CfnAuthorizer {
+  constructor(scope: Construct, id: string, props: CfnAuthorizerProps) {
+    super(scope, id, props);
+  }
+
+  /**
+   * Required by IAuthorizer to get a reference to a custom authorizer function
+   */
+  get authorizerId(): string {
+      return this.ref;
+  }
 }
